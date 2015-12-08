067cd1b3c8f18ca8802ea3d14a9cf2409446084f
Ngoguey42 <ngoguey@student.42.fr>
2015-12-08 19:23:39 +0100

    update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   main.cpp                                           :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2015/12/08 19:07:00 by ngoguey           #+#    #+#             //
+//   Updated: 2015/12/08 19:23:22 by ngoguey          ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
 
 #include <iostream>
 #include <cstdint>
 #include <type_traits>
 #include <algorithm>
-
+#include <cmath>
 
 enum class eOperandType
 {
@@ -26,16 +37,15 @@ template <>			struct operand_enum<float>
 template <>			struct operand_enum<double>
 	: std::integral_constant<eOperandType, eOperandType::Double> {};
 
-
 class IOperand {
 public:
 	// virtual int getPrecision( void ) const = 0; // Precision of the type of the instance
 	virtual eOperandType getType( void ) const = 0; // Type of the instance
 	virtual IOperand const * operator+( IOperand const & rhs ) const = 0; // Sum
-	// virtual IOperand const * operator-( IOperand const & rhs ) const = 0; // Difference
-	// virtual IOperand const * operator*( IOperand const & rhs ) const = 0; // Product
-	// virtual IOperand const * operator/( IOperand const & rhs ) const = 0; // Quotient
-	// virtual IOperand const * operator%( IOperand const & rhs ) const = 0; // Modulo
+	virtual IOperand const * operator-( IOperand const & rhs ) const = 0; // Difference
+	virtual IOperand const * operator*( IOperand const & rhs ) const = 0; // Product
+	virtual IOperand const * operator/( IOperand const & rhs ) const = 0; // Quotient
+	virtual IOperand const * operator%( IOperand const & rhs ) const = 0; // Modulo
 	virtual std::string const & toString( void ) const = 0; // String representation of the instance
 	virtual ~IOperand( void ) {}
 };
@@ -61,16 +71,64 @@ public:
 
 };
 
+
+template <class T>
 class Evalexpr
 {
+public:
+	enum eOperation
+	{
+		Add = 0,
+		Sub,
+		Div,
+		Mul,
+		Mod
+	};
+
+private:
+	static std::string		conv(T v) {
+		return std::to_string(v);
+	}
+
+# define OK_IF(PRED) typename std::enable_if<PRED>::type* = nullptr
+# define ISFLOAT(V) std::is_floating_point<V>::value
+
+	template <class U = T, OK_IF(ISFLOAT(U))>
+	static T				conv(std::string const &v) {
+		return std::atof(v.c_str());
+	}
+	template <class U = T, OK_IF(!ISFLOAT(U))>
+	static T				conv(std::string const &v) {
+		return std::atol(v.c_str());
+	}
+
+# undef OF_IF
+# undef ISFLOAT
+
+	static const std::function< T(T, T) > operations[5];
 
 public:
-	static std::string		eval(std::string const &str, eOperandType outType) {
-		(void)str;
-		(void)outType;
-		return "";
+
+	static std::string		eval(
+		std::string const &lhs, eOperation o, std::string const &rhs) {
+		T const		res = operations[o](conv(lhs), conv(rhs));
+
+		return conv(res);
 	}
 };
+
+template <class T>
+const std::function< T(T, T) >		Evalexpr<T>::operations[5] = {
+	std::plus<T>(),
+	std::minus<T>(),
+	std::multiplies<T>(),
+	std::divides<T>(),
+	[](T a, T b){ return std::fmod(a, b); },
+};
+// fmod has a special overload working with integer types
+// Promoted		fmod( Arithmetic1 x, Arithmetic2 y );
+// http://en.cppreference.com/w/cpp/numeric/math/fmod
+
 
 
 template <class T, eOperandType TEnumVal = operand_enum<T>::value>
@@ -83,7 +141,7 @@ public:
 
 	~Operand() {}
 	Operand(OpFactory const &fact, std::string const &str)
-		: _fact(fact), _val(Evalexpr::eval(str, TEnumVal)) {}
+		: _fact(fact), _val(str) {}
 
 	Operand() = delete;
 	Operand(Operand const &src) = delete;
@@ -100,11 +158,54 @@ public:
 	}
 
 	IOperand const *operator+( IOperand const & rhs) const override {
-		std::string const	expr = _val + "+" + rhs.toString();
+		eOperandType const	dstType = std::max(rhs.getType(), TEnumVal);
 
-		return _fact.createOperand(std::max(rhs.getType(), TEnumVal), expr);
+		if (dstType != TEnumVal)
+			return rhs + *this;
+		else
+			return _fact.createOperand(
+				dstType, Evalexpr<T>::eval(_val, Evalexpr<T>::Add, rhs.toString()));
+	}
+
+	IOperand const *operator-( IOperand const & rhs) const override {
+		eOperandType const	dstType = std::max(rhs.getType(), TEnumVal);
+
+		if (dstType != TEnumVal)
+			return rhs + *this;
+		else
+			return _fact.createOperand(
+				dstType, Evalexpr<T>::eval(_val, Evalexpr<T>::Sub, rhs.toString()));
+	}
+	IOperand const *operator*( IOperand const & rhs) const override {
+		eOperandType const	dstType = std::max(rhs.getType(), TEnumVal);
+
+		if (dstType != TEnumVal)
+			return rhs + *this;
+		else
+			return _fact.createOperand(
+				dstType, Evalexpr<T>::eval(_val, Evalexpr<T>::Mul, rhs.toString()));
+	}
+	IOperand const *operator/( IOperand const & rhs) const override {
+		eOperandType const	dstType = std::max(rhs.getType(), TEnumVal);
+
+		if (dstType != TEnumVal)
+			return rhs + *this;
+		else
+			return _fact.createOperand(
+				dstType, Evalexpr<T>::eval(_val, Evalexpr<T>::Div, rhs.toString()));
+	}
+	IOperand const *operator%( IOperand const & rhs) const override {
+		eOperandType const	dstType = std::max(rhs.getType(), TEnumVal);
+
+		if (dstType != TEnumVal)
+			return rhs + *this;
+		else
+			return _fact.createOperand(
+				dstType, Evalexpr<T>::eval(_val, Evalexpr<T>::Mod, rhs.toString()));
 	}
 };
+
+
 
 
 
@@ -138,19 +239,6 @@ IOperand const * OpFactory::createOperand( eOperandType type, std::string const 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 // IOperand const * OpFactory::createOperand( eOperandType type, std::string const & value ) const;
 
 
@@ -158,7 +246,7 @@ IOperand const * OpFactory::createOperand( eOperandType type, std::string const 
 namespace internal // ~~~~~~~~~~~~~~~~~~~~~~~~~ //
 { // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
-template <class A, class B, bool AF, bool BF>
+template <class A, class B, bool, bool>
 struct result_type {
 	typedef typename std::conditional<(sizeof(A) > sizeof(B)), A, B>::type type;
 };
@@ -187,17 +275,17 @@ struct result_type : public internal::result_type<A, B, ISFLOAT(A), ISFLOAT(B)>
 #define TESTFUN(T)								\
 	void	testfun(T)							\
 	{											\
-		std::cout << #T << std::endl;			\
+		std::cout << #T << std::endl;	\
 		return ;								\
 	}
 
 TESTFUN(int8_t)
-	TESTFUN(int16_t)
-	TESTFUN(int32_t)
-	TESTFUN(float)
-	TESTFUN(double)
+TESTFUN(int16_t)
+TESTFUN(int32_t)
+TESTFUN(float)
+TESTFUN(double)
 
-	int							main(void)
+int							main(void)
 {
 	int8_t		i8 = 8;
 	int16_t		i16 = 16;
@@ -206,39 +294,37 @@ TESTFUN(int8_t)
 	double		d = 80.;
 
 // #define TEST(A, B) testfun(A + B)
-#define TEST(A, B) testfun(static_cast<result_type<decltype(A), decltype(B)>::type>(A + B))
+#define TEST(A, B) testfun(static_cast< result_type<decltype(A), decltype(B)>::type >(A + B))
 
-		TEST(i8, i8);
-		TEST(i8, i16);
-		TEST(i8, i32);
-		TEST(i8, f);
-		TEST(i8, d);
-		std::cout << "" << std::endl;
-		TEST(i16, i8);
-		TEST(i16, i16);
-		TEST(i16, i32);
-		TEST(i16, f);
-		TEST(i16, d);
-		std::cout << "" << std::endl;
-		TEST(i32, i8);
-		TEST(i32, i16);
-		TEST(i32, i32);
-		TEST(i32, f);
-		TEST(i32, d);
-		std::cout << "" << std::endl;
-		TEST(f, i8);
-		TEST(f, i16);
-		TEST(f, i32);
-		TEST(f, f);
-		TEST(f, d);
-		std::cout << "" << std::endl;
-		TEST(d, i8);
-		TEST(d, i16);
-		TEST(d, i32);
-		TEST(d, f);
-		TEST(d, d);
-		std::cout << "" << std::endl;
-
-
-		return (0);
+	TEST(i8, i8);
+	TEST(i8, i16);
+	TEST(i8, i32);
+	TEST(i8, f);
+	TEST(i8, d);
+	std::cout << "" << std::endl;
+	TEST(i16, i8);
+	TEST(i16, i16);
+	TEST(i16, i32);
+	TEST(i16, f);
+	TEST(i16, d);
+	std::cout << "" << std::endl;
+	TEST(i32, i8);
+	TEST(i32, i16);
+	TEST(i32, i32);
+	TEST(i32, f);
+	TEST(i32, d);
+	std::cout << "" << std::endl;
+	TEST(f, i8);
+	TEST(f, i16);
+	TEST(f, i32);
+	TEST(f, f);
+	TEST(f, d);
+	std::cout << "" << std::endl;
+	TEST(d, i8);
+	TEST(d, i16);
+	TEST(d, i32);
+	TEST(d, f);
+	TEST(d, d);
+	std::cout << "" << std::endl;
+	return (0);
 }
