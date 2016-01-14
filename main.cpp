3d7ad87aa6314e4cd4feaf350ecbd0f0f07c34af
Ngoguey42 <ngoguey@student.42.fr>
2016-01-14 20:18:31 +0100

    added phat tests

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 //   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/12/08 19:29:27 by ngoguey           #+#    #+#             //
-//   Updated: 2016/01/14 17:29:47 by ngoguey          ###   ########.fr       //
+//   Updated: 2016/01/14 19:52:26 by ngoguey          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -146,44 +146,18 @@ void add_dump_pop(VMStack &vmst)
 
 
 
-int							main(void)
+int							mainn(void)
 {
 	OpFactory fact;
 	VMStack vmst(fact);
 
-	DUMP(float, min);
-	DUMP(float, lowest);
-	DUMP(float, max);
-	DUMP(float, epsilon);
-	DUMP(float, round_error);
-	DUMP(float, infinity);
-	DUMP(float, denorm_min);
-	std::cout << std::endl;
-	DUMP(double, min);
-	DUMP(double, lowest);
-	DUMP(double, max);
-	DUMP(double, epsilon);
-	DUMP(double, round_error);
-	DUMP(double, infinity);
-	DUMP(double, denorm_min);
-	std::cout << std::endl;
 
-
-	VMStack::actmap.at("push")(&vmst, "Float(1000000000)");
-	VMStack::actmap.at("push")(&vmst, "Float(+0)");
-	VMStack::actmap.at("add")(&vmst, "");
-	VMStack::actmap.at("push")(&vmst, "Float(10000000000)");
-	VMStack::actmap.at("push")(&vmst, "Float(+0)");
-	VMStack::actmap.at("add")(&vmst, "");
-	VMStack::actmap.at("push")(&vmst, "Float(100000000000)");
-	VMStack::actmap.at("push")(&vmst, "Float(+0)");
-	VMStack::actmap.at("add")(&vmst, "");
-	VMStack::actmap.at("push")(&vmst, "Float(1000000000000)");
-	VMStack::actmap.at("push")(&vmst, "Float(+0)");
-	VMStack::actmap.at("add")(&vmst, "");
-	VMStack::actmap.at("push")(&vmst, "Float(10000000000000)");
-	VMStack::actmap.at("push")(&vmst, "Float(+0)");
-	VMStack::actmap.at("add")(&vmst, "");
+	for (int i = 0; i < 45; i++)
+	{
+		VMStack::actmap.at("push")(&vmst, std::string("Float(1") + std::string(i, '0') + ")");
+		VMStack::actmap.at("push")(&vmst, "Float(+0)");
+		VMStack::actmap.at("add")(&vmst, "");
+	}
 	VMStack::actmap.at("dump")(&vmst, "");
 	// add_dump_pop(vmst);
 	std::cout << "" << std::endl;
@@ -210,4 +184,172 @@ int							main(void)
 	VMStack::actmap.at("pop")(&vmst, "");
 	std::cout << "" << std::endl;
 	return (0);
+}
+
+#include <iostream>
+#include <cfenv>
+#include <cmath>
+#include <cerrno>
+#include <cstring>
+// #pragma STDC FENV_ACCESS ON
+int mainnn()
+{
+	std::feclearexcept(FE_ALL_EXCEPT);
+	errno = 0;
+	std::cout <<  "log(0) = " << std::log(0) << '\n';
+	if(errno == ERANGE)
+		std::cout << "errno = ERANGE (" << std::strerror(errno) << ")\n";
+	if(std::fetestexcept(FE_DIVBYZERO))
+		std::cout << "FE_DIVBYZERO (pole error) reported\n";
+	return 0;
+}
+
+
+
+template <class T>
+std::string		type_of_expr(T const &) {
+	return typeid(T).name();
+}
+
+
+#define I8(V) static_cast<int8_t>((V))
+#define I16(V) static_cast<int16_t>((V))
+#define I32(V) static_cast<int16_t>((V))
+
+
+void dumpFeExcept(void)
+{
+#define REPORT(FLAG)									\
+	std::cout << std::setw(13) << #FLAG;				\
+	if (std::fetestexcept((FLAG)))						\
+		std::cout << " \033[31m1\033[0m" << std::endl;	\
+	else												\
+		std::cout << " 0" << std::endl
+	REPORT(FE_DIVBYZERO);
+	REPORT(FE_INEXACT);
+	REPORT(FE_INVALID);
+	REPORT(FE_OVERFLOW);
+	REPORT(FE_UNDERFLOW);
+	// REPORT(FE_ALL_EXCEPT);
+	std::cout << std::endl;
+	return ;
+}
+
+
+#define TESTA(EXPR)														\
+	do {																\
+		std::feclearexcept(FE_ALL_EXCEPT);								\
+		std::cout << #EXPR << std::endl;								\
+		EXPR;															\
+		bool isflt = std::is_floating_point<decltype(val)>::value;		\
+		std::cout << " val -> " << (isflt ? val : (int)val) << std::endl; \
+		dumpFeExcept();													\
+	} while (0)
+
+
+
+#define TESTB(EXPR)														\
+	std::cout << #EXPR << " gave type: " << type_of_expr(EXPR) << '\n'
+
+
+
+#define DMAX std::numeric_limits<double>::max()
+#define DMIN std::numeric_limits<double>::min()
+#define DLOWEST std::numeric_limits<double>::lowest()
+
+int main()
+{
+	DUMP(double, min);
+	DUMP(double, lowest);
+	DUMP(double, max);
+	DUMP(double, epsilon);
+	DUMP(double, round_error);
+	DUMP(double, infinity);
+	DUMP(double, denorm_min);
+	std::cout << std::endl;
+
+
+#define DEFINE(TYPE, FUN)												\
+	std::cout << "defined: " #TYPE #FUN << std::endl;				\
+	volatile const TYPE TYPE##FUN = std::numeric_limits<TYPE>::FUN()
+
+	DEFINE(double, max);
+	DEFINE(float, max);
+	DEFINE(double, lowest);
+	DEFINE(float, lowest);
+	DEFINE(double, min);
+	DEFINE(float, min);
+	DEFINE(double, denorm_min);
+	DEFINE(float, denorm_min);
+
+	volatile const double zero = 0.0;
+	volatile const double one = 1.0;
+	volatile const double dmax = DMAX;
+
+
+	// double (*ftsqrt)(double, double) = static_cast<double (*)(double, double)>(std::pow);
+
+	TESTA(const double val = doublemax + doublemax);
+	TESTA(const double val = doublelowest + doublelowest);
+	// TESTA(const double val = doublemin);
+	// TESTA(const double val = doublemin / 2);
+	// TESTA(const double val = doubledenorm_min);
+	TESTA(const double val = doubledenorm_min / 2);
+	// TESTA(const double val = 0. / 0.);
+	// TESTA(const double val = zero / zero);
+	// TESTA(const double val = zero + zero);
+	// TESTA(const double val = -zero);
+	// TESTA(const double val = zero);
+	// TESTA(const double val = one);
+	// TESTA(const double val = 1.);
+
+	// TESTA(const float val = floatmax);
+	// TESTA(const float val = floatmax + floatmax);
+	// TESTA(const double val = doublelowest);
+	// TESTA(const float val = floatlowest);
+	// TESTA(const float val = floatlowest + floatlowest);
+	// TESTA(const float val = floatlowest - 1.f);
+	// TESTA(const double val = dmax + dmax);
+	// TESTA(const double val = DMAX + DMAX);
+	// TESTA(volatile const double val = DMAX + DMAX);
+	// TESTA(volatile const double val = dmax + dmax);
+	// double (*ftsqrt)(double) = static_cast<double (*)(double)>(std::sqrt);
+
+	// TESTA(volatile const double val = std::plus<double>()(DMAX, DMAX));
+	// TESTA(volatile const double val = std::plus<double>()(DMAX, 1.));
+	// TESTA(volatile const double val = DMAX + DMAX);
+	// TESTA(volatile const double val = std::pow<double>(std::numeric_limits<double>::max(), 2));
+	// TESTA(volatile const double val = DMAX);
+	// TESTA(volatile const double val = DMIN);
+	// TESTA(volatile const double val = DMIN * 1000);
+	// TESTA(volatile const double val = ftsqrt(DMIN, 2.));
+	// TESTA(volatile const double val = ftsqrt(DMIN * 10000000000., 2.));
+	// TESTA(volatile const double val = std::sqrt<double>(DLOWEST));
+
+
+	return 0;
+
+	// TESTA(volatile const double val = one / zero);
+	// TESTA(volatile const float val = zero / zero);
+	// TESTA(volatile const double val = 2.);
+	// TESTA(volatile const double val = one / one);
+	// TESTA(volatile const int8_t val = I8(I8(-128) - I8(1)));
+	// TESTA(volatile const float val = std::numeric_limits<float>::max());
+	TESTA(volatile const double val = std::numeric_limits<double>::min() * std::numeric_limits<double>::min());
+	// TESTA(volatile const float val = std::numeric_limits<float>::max() * 2.f);
+	// TESTA(volatile const float val = std::numeric_limits<float>::max() + 10000000000000000000000000000000000000.f);
+
+	DUMP(float, min);
+	DUMP(float, lowest);
+	DUMP(float, max);
+	DUMP(float, epsilon);
+	DUMP(float, round_error);
+	DUMP(float, infinity);
+	DUMP(float, denorm_min);
+	std::cout << std::endl;
+
+	// std::feclearexcept(FE_ALL_EXCEPT);
+	// std::cout << "sqrt(-1) = " << std::sqrt(-1) << '\n';
+	// dumpFeExcept();
+	return 0;
 }
