bc30ad4d66ef88a3b4d253b6f8ecb5e9c9aaefd4
Ngoguey42 <ngoguey@student.42.fr>
2016-01-22 16:23:42 +0100

    implemented FloatOperation

--- Evalexpr.hpp
+++ Evalexpr.hpp
@@ -6,7 +6,7 @@
 //   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/12/09 11:02:52 by ngoguey           #+#    #+#             //
-//   Updated: 2016/01/22 13:41:29 by ngoguey          ###   ########.fr       //
+//   Updated: 2016/01/22 16:23:17 by ngoguey          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -15,6 +15,7 @@
 
 #include <cstdlib>
 #include <functional>
+#include <cfenv>
 #include <cmath>
 
 # define OK_IF(PRED) typename std::enable_if<PRED>::type* = nullptr
@@ -39,6 +40,123 @@
 **  Mul std::multiplies	overflow_error
 **  Mod std::modulus	domain_error
 */
+
+namespace tmp // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+{ // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+enum eOperation
+{
+	Add = 0,
+	Sub,
+	Div,
+	Mul,
+	Mod
+};
+
+namespace detail // ~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+{ // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+// Is operand Div or Mod ================================ //
+template <eOperation> struct IsDivOrMod
+	: std::integral_constant<bool, false>
+{};
+template <> struct IsDivOrMod<eOperation::Div>
+	: std::integral_constant<bool, true>
+{};
+template <> struct IsDivOrMod<eOperation::Mod>
+	: std::integral_constant<bool, true>
+{};
+
+// Operations =========================================== //
+
+template <class T, eOperation> struct FloatOperation;
+
+template <class T> struct FloatOperation<T, eOperation::Add> {
+	static const auto func = std::plus<T>();
+};
+template <class T> struct FloatOperation<T, eOperation::Sub> {
+	static const auto func = std::minus<T>();
+};
+template <class T> struct FloatOperation<T, eOperation::Div> {
+	static const auto func = std::divides<T>();
+};
+template <class T> struct FloatOperation<T, eOperation::Mul> {
+	static const auto func = std::multiplies<T>();
+};
+template <class T> struct FloatOperation<T, eOperation::Mod> {
+	static const auto func = static_cast< T(T, T) >(std::fmod);
+};
+
+// Evalexpr implementations ============================= //
+template <class T, eOperation Operation, bool IsFloat, bool IsDivOrMod>
+class Evalexpr;
+
+template <class T, eOperation Operation, bool IsDivOrMod>
+class Evalexpr<T, Operation, true, IsDivOrMod>
+{
+
+};
+
+template <class T>
+class Evalexpr<T, eOperation::Mod, true, true>
+{
+
+};
+
+template <class T, eOperation Operation>
+class Evalexpr<T, Operation, false, false>
+{
+
+};
+
+template <class T, eOperation Operation, bool IsDivOrMod>
+class Evalexpr<T, Operation, false, IsDivOrMod>
+{
+	T operator ()(T const &x, T const &y) {
+
+		std::function< T(T const &, T const &) > const arr[] = {
+			[eOperation::Mod] = std::fmod,
+			[eOperation::Div] = std::divides<T>(),
+		};
+		T ret;
+
+		std::feclearexcept(FE_ALL_EXCEPT);
+		// ret = arr[Operation](x, y);
+		ret = FloatOperation<T, Operation>(x, y);
+		// if (std::fpclassify() == FP_ZERO)
+		// 	throw std::domain_error("msg");
+		return ret;
+	}
+};
+
+
+}; // ~~~~~~~~~~~~~~~~~ END OF NAMESPACE DETAIL //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+
+template <class T, eOperation Operation>
+class Evalexpr : detail::Evalexpr<
+	T, Operation, ISFLOAT(T), detail::IsDivOrMod<Operation>::value>
+{};
+
+static inline void lol(void) {
+	Evalexpr<float, eOperation::Add>();
+	Evalexpr<float, eOperation::Sub>();
+	Evalexpr<float, eOperation::Div>();
+	Evalexpr<float, eOperation::Mul>();
+	Evalexpr<float, eOperation::Mod>();
+	Evalexpr<short, eOperation::Add>();
+	Evalexpr<short, eOperation::Sub>();
+	Evalexpr<short, eOperation::Div>();
+	Evalexpr<short, eOperation::Mul>();
+	Evalexpr<short, eOperation::Mod>();
+
+}
+
+
+}; // ~~~~~~~~~~~~~~~~~~~~ END OF NAMESPACE TMP //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
 
 template <class T>
 class Evalexpr
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 //   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/12/08 19:29:27 by ngoguey           #+#    #+#             //
-//   Updated: 2016/01/22 13:32:20 by ngoguey          ###   ########.fr       //
+//   Updated: 2016/01/22 14:11:02 by ngoguey          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -48,9 +48,9 @@ void add_dump_pop(VMStack &vmst)
 
 void dumpFeExcept(void)
 {
-#define REPORT(FLAG)									\
-	if (std::fetestexcept((FLAG)))						\
-		std::cout << std::setw(13) << #FLAG <<			\
+#define REPORT(FLAG)							\
+	if (std::fetestexcept((FLAG)))				\
+		std::cout << std::setw(13) << #FLAG <<	\
 			" \033[31m1\033[0m" << std::endl
 	REPORT(FE_DIVBYZERO);
 	REPORT(FE_INVALID);
@@ -71,7 +71,109 @@ void dumpFeExcept(void)
 	} while (0)
 
 
-int main()
+
+char plus(char a, char b)
+{
+	char const res = a + b;
+
+	if (b > char(0))
+	{
+		if (res < a)
+			throw std::overflow_error("bordel!");
+	}
+	else
+	{
+		if (res > a)
+			throw std::overflow_error("bordel!");
+	}
+	return res;
+}
+
+char minus(char a, char b)
+{
+	char const res = a - b;
+
+	if (b > char(0))
+	{
+		if (res > a)
+			throw std::overflow_error("bordel!");
+	}
+	else
+	{
+		if (res < a)
+			throw std::overflow_error("bordel!");
+	}
+	return res;
+}
+
+char multiplies(char a, char b)
+{
+	char const res = a * b;
+
+	if ((a > char(0)) == (b > char(0)))
+	{
+		if (res < a)
+			throw std::overflow_error("bordel!");
+	}
+	else
+	{
+		if (res > a)
+			throw std::overflow_error("bordel!");
+	}
+	return res;
+}
+
+
+void test(char (*fc)(char, char), std::function< int(int, int) > fi)
+{
+	char i = -128;
+	int nsucc = 0;
+
+	do {
+		char j = -128;
+		// std::cout << int(i) << " ";
+		do {
+			int const resi = fi(i, j);
+			char res;
+			bool thrown;
+			bool overflow;
+
+			try {
+				res = fc(i, j);
+				thrown = false;
+			} catch (...) {
+				thrown = true;
+			}
+			overflow = resi != int(res);
+
+			if (overflow != thrown)
+			{
+				std::cout << "thrown(" << std::boolalpha << thrown
+						  << ") at op(" << int(i) << ", " << int (j)
+						  << ")"
+						  << std::endl;
+			}
+			else
+				nsucc++;
+		} while (j++ < 127);
+	} while (i++ < 127);
+	std::cout << "Done " << nsucc << " success" << std::endl;
+	return ;
+}
+
+
+int	main(void)
+{
+
+	test(&minus, std::minus<int>());
+	test(&plus, std::plus<int>());
+	test(&multiplies, std::multiplies<int>());
+
+	return (0);
+}
+
+
+int mainold()
 {
 	DUMP(double, min);
 	DUMP(double, lowest);
