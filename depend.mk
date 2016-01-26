c35d2d548b2ed0f21e31cea892e71b819795dc28
Ngoguey42 <ngoguey@student.42.fr>
2016-01-26 15:55:11 +0100

    done regex checker for input line

--- depend.mk
+++ depend.mk
@@ -1,5 +1,5 @@
 O_FILES :=	o/srcs/InputHandler.o o/srcs/VMStack.o o/srcs/arith_wrappers.o \
-			o/srcs_test/test1.o
+			o/srcs/cfg_checker.o o/srcs_test/test1.o
 
 LIBS_DEPEND := 
 
@@ -16,4 +16,5 @@ o/srcs/VMStack.o: srcs/VMStack.cpp include/MStack.hpp include/VMStack.hpp \
 o/srcs/arith_wrappers.o: srcs/arith_wrappers.cpp include/arith_operations.hpp \
 	include/arith_serializations.hpp include/arith_types.hpp \
 	include/arith_wrappers.hpp | o/srcs/
+o/srcs/cfg_checker.o: srcs/cfg_checker.cpp | o/srcs/
 o/srcs_test/test1.o: srcs_test/test1.cpp | o/srcs_test/
---
+++ srcs/cfg_checker.cpp
@@ -0,0 +1,103 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   cfg_checker.cpp                                    :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2016/01/26 14:23:16 by ngoguey           #+#    #+#             //
+//   Updated: 2016/01/26 15:52:12 by ngoguey          ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+#include <iostream> //debug
+#include <regex> //debug
+
+static char const *n{"[-]?[0-9]+"};
+static char const *z{"[-]?[0-9]+[.][0-9]+"};
+static char const *blank_star{"[ \t]*"};
+static char const *blank_plus{"[ \t]+"};
+static char const *termN[] = {
+	"int8", "int16", "int32"
+};
+static char const *termZ[] = {
+	"float", "double"
+};
+static char const *terma[] = {
+	"pop", "dump", "add", "sub", "mul", "div", "mod", "print", "exit"
+};
+static char const *termb[] = {
+	"push", "assert"
+};
+
+static std::string make_value(void)
+{
+	std::string ret;
+
+	ret += "(";
+	for (char const *term : termN)
+	{
+		if (term == *termN)
+			ret += "(";
+		else
+			ret += "|(";
+		ret += term;
+		ret += "[(]";
+		ret += n;
+		ret += "[)]";
+		ret += ")";
+	}
+	for (char const *term : termZ)
+	{
+		ret += "|(";
+		ret += term;
+		ret += "[(]";
+		ret += z;
+		ret += "[)]";
+		ret += ")";
+	}
+	ret += ")";
+	return ret;
+}
+
+static std::string make_instr(void)
+{
+	std::string const value{make_value()};
+	std::string ret;
+
+	ret += blank_star;
+	ret += "(";
+	for (char const *term : terma)
+	{
+		if (term == *terma)
+			ret += "(";
+		else
+			ret += "|(";
+		ret += term;
+		ret += ")";
+	}
+	for (char const *term : termb)
+	{
+		ret += "|(";
+		ret += term;
+		ret += blank_plus;
+		ret += value;
+		ret += ")";
+	}
+	ret += ")";
+	ret += blank_star;
+	return ret;
+}
+
+static int dobordel()
+{
+	std::string const inst{make_instr()};
+	std::regex const VALUE{inst};
+
+	std::cout << inst << std::endl;
+	std::cout << std::regex_match("pop", VALUE) << std::endl;
+	std::cout << std::regex_match("push double(-999.0)", VALUE) << std::endl;
+	return 0;
+}
+
+static int lol = dobordel();
