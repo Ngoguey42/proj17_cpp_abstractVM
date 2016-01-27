03a0a66bc429334f14708e0ec018ce8dc9f1153e
Ngoguey42 <ngoguey@student.42.fr>
2016-01-27 19:43:33 +0100

    added srcs_test/testConf.hpp; underflow detected

--- srcs_test/test1.cpp
+++ srcs_test/test1.cpp
@@ -6,7 +6,7 @@
 //   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2016/01/25 20:06:12 by ngoguey           #+#    #+#             //
-//   Updated: 2016/01/27 19:18:07 by ngoguey          ###   ########.fr       //
+//   Updated: 2016/01/27 19:42:59 by ngoguey          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -18,7 +18,9 @@ using namespace boost::unit_test;
 using boost::test_tools::output_test_stream;
 
 #include <deque>
+#include <type_traits>
 
+#include "testConf.hpp"
 #include "testTools.hpp"
 #include "Controller.hpp"
 
@@ -32,6 +34,7 @@ struct Fix {
 	long double fx_ref_valld;
 	std::string fx_ref_valstr;
 	std::string fx_ref_valtypestr;
+	bool fx_isfloat;
 
 	template <class T>
 	void fx_init(T const &val, std::function<std::string(T const &)> fn) {
@@ -40,21 +43,21 @@ struct Fix {
 		fx_ref_valstr = fn(val);
 		fx_ref_valtypestr = std::string(TypeToString<T>::name)
 			+ "(" + fx_ref_valstr + ")";
+		fx_isfloat = std::is_floating_point<T>::value;
 		BOOST_TEST_MESSAGE(CYEL + "preparing some test with "
 						   + truncate(fx_ref_valtypestr) + CEND);
 		return ;
 	}
 
 	template <class T>
-	void fx_run(void) {
+	void fx_run_paad(void) { // paad Push Add0 Assert Dump
+
 		BOOST_TEST_MESSAGE(
 			TAB + CYEL + "push_add0_assert_dump operations" + CEND);
 
 		std::stringstream oss;
 		cout_redirect guardout(fx_output.rdbuf());
 		cin_redirect guardin(oss.rdbuf());
-
-		fx_output.str(""); //reset output stream
 
 		oss << "push ";
 		oss << fx_ref_valtypestr;
@@ -70,11 +73,38 @@ struct Fix {
 		oss << "dump\n";
 		oss << "exit\n";
 		oss << ";;";
+		RUN_ABSTRACT_VM;
+		// Controller()(1, nullptr); //run abstract vm program with ac=1 av=nullptr
+		return ;
+	}
+
+	template <class T>
+	void fx_run_selfmult(void) { // paad Push Add0 Assert Dump
+
+		BOOST_TEST_MESSAGE(TAB + CYEL + "push_push_mul operations" + CEND);
+
+		std::stringstream oss;
+		cout_redirect guardout(fx_output.rdbuf());
+		cin_redirect guardin(oss.rdbuf());
+
+		oss << "push ";
+		oss << fx_ref_valtypestr;
+		oss << "\n";
+
+		oss << "push ";
+		oss << fx_ref_valtypestr;
+		oss << "\n";
+
+		oss << "mul\n";
+
+		oss << "exit\n";
+		oss << ";;";
 		Controller()(1, nullptr); //run abstract vm program with ac=1 av=nullptr
 		return ;
 	}
+
 	template <class T>
-	void fx_validate(void) {
+	void fx_validate_ok(void) {
 
 		long double dumped_valld;
 
@@ -83,13 +113,21 @@ struct Fix {
 		return ;
 	}
 
+	template <class T>
+	void fx_validate_underflow(void) {
+
+		BOOST_CHECK(UNDERFLOWED(fx_output.str()));
+		return ;
+	}
 };
 
-typedef boost::mpl::list<int8_t, int16_t, int32_t, float, double> test_types;
+typedef boost::mpl::list<int8_t, int16_t, int32_t, float, double> all_types;
+typedef boost::mpl::list<float, double> fp_types;
+typedef boost::mpl::list<int8_t, int16_t, int32_t> i_types;
 
 BOOST_FIXTURE_TEST_SUITE(s, Fix)
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(max, T, test_types)
+BOOST_AUTO_TEST_CASE_TEMPLATE(max, T, all_types)
 {
 	fx_init<T>(std::numeric_limits<T>::max(), [](T const &val){
 		std::stringstream iss;
@@ -98,11 +136,11 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(max, T, test_types)
 		nbrToStream(val, iss);
 		return iss.str();
 		});
-	fx_run<T>();
-	fx_validate<T>();
+	fx_run_paad<T>();
+	fx_validate_ok<T>();
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(min, T, test_types)
+BOOST_AUTO_TEST_CASE_TEMPLATE(min, T, all_types)
 {
 	fx_init<T>(std::numeric_limits<T>::denorm_min(), [](T const &val){
 		std::stringstream iss;
@@ -114,11 +152,11 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(min, T, test_types)
 		nbrToStream(val, iss);
 		return iss.str();
 		});
-	fx_run<T>();
-	fx_validate<T>();
+	fx_run_paad<T>();
+	fx_validate_ok<T>();
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(lowest, T, test_types)
+BOOST_AUTO_TEST_CASE_TEMPLATE(lowest, T, all_types)
 {
 	fx_init<T>(std::numeric_limits<T>::lowest(), [](T const &val){
 		std::stringstream iss;
@@ -127,8 +165,25 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(lowest, T, test_types)
 		nbrToStream(val, iss);
 		return iss.str();
 		});
-	fx_run<T>();
-	fx_validate<T>();
+	fx_run_paad<T>();
+	fx_validate_ok<T>();
 }
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(min_underflow, T, fp_types)
+{
+	fx_init<T>(std::numeric_limits<T>::denorm_min(), [](T const &val){
+		std::stringstream iss;
+		int tmp;
+
+		tmp = -floor(log10(std::abs(std::numeric_limits<T>::denorm_min())));
+		tmp += std::numeric_limits<T>::max_digits10 - 1;
+		iss << std::fixed << std::noshowpos << std::setprecision(tmp);
+		nbrToStream(val, iss);
+		return iss.str();
+		});
+	fx_run_selfmult<T>();
+	fx_validate_underflow<T>();
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
---
+++ srcs_test/testConf.hpp
@@ -0,0 +1,24 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   testConf.hpp                                       :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2016/01/27 19:34:12 by ngoguey           #+#    #+#             //
+//   Updated: 2016/01/27 19:42:34 by ngoguey          ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+#ifndef TESTCONF_HPP
+# define TESTCONF_HPP
+
+# include <regex>
+
+#define RUN_ABSTRACT_VM							\
+	Controller()(1, nullptr)
+
+#define UNDERFLOWED(OUTPUT)								\
+	std::regex_search((OUTPUT), std::regex("underflow_error"))
+
+#endif /* ****************************************************** TESTCONF_HPP */
